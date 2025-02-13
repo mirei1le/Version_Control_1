786ea0855e84ae62e8931c3a01fd706ef194b286
mirei1le <B00162935@mytudublin.ie>
2025-02-13 15:00:22 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Page</title>
+    <link rel="stylesheet" href="/style.css">
+
+</head>
 <body>
 
-<h1>My first PHP page</h1>
+    <h1>My first PHP page</h1>
 
-<?php
-echo "This is Shopping!";
-?>
+    <?php
+    echo "Shopping is fun!";
+    ?>
 
 </body>
 </html>
